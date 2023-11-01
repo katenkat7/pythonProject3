cf87543df44cee63cb0bb899c87a3a9eaf534619
katenkat7 <katenkat7@gmail.com>
2023-11-02 02:17:53 +0300

    init

--- p1/stream.py
+++ p1/stream.py
@@ -1,105 +1,69 @@
-"""
-This is a demo of the mitosheet library. It is a simple streamlit app that allows you to import data and clean it using mitosheet.
-"""
-
-import streamlit as st
-import pandas as pd
-from mitosheet.streamlit.v1 import spreadsheet
-import analytics
-
-analytics.write_key = '6I7ptc5wcIGC4WZ0N1t0NXvvAbjRGUgX'
-
-st.set_page_config(layout="wide")
-st.title("Data Cleaning Verification")
-
-st.markdown("""
-This app only allows you to download data after it passes a series of data quality checks. After importing data, the app will run a series of checks against your data and prompt you with a set of data cleaning steps. 
-
-To use the app, follow the mitosheet below:
-1. Click **Import** > **Import Files** and select an XLSX file from the `data` folder.
-2. Click the **Import Button**, and configure the import to skip rows depending on the file you choose.
-3. Use the Mitosheet to clean the data according to the prompts.
-4. Once all of the checks pass, download the csv file.
-
-This app is meant to demo the mitosheet library. Learn more [here](https://trymito.io).
-""")
-
-CHECKS_AND_ERRORS = [
-    # First column is issue date
-    (
-        lambda df: df.columns[0] != 'issue date',
-        'Please edit the first column name to "issue date".',
-        'You can do this by double clicking on the column name.'
-    ),
-    # Correct dtype
-    (
-        lambda df: df["is2ype of the "issue date" column to datetime.',
-        'You can do this by clicking on the Filter icon, and then selecting "datetime" from the "dtype" dropdown.'
-
-    ),
-    # No null values
-    (
-        lambda df: df["issue date"].isnull().sum() > 0,
-        'Please filter out all null values from the issue date column.',
-        'You can do this by clicking on the filter icon in the issue date column header, and adding an "Is Not Empty" filter.'
-    ),
-    # Delete the Notes column
-    (
-        lambda df: "Notes" in df.columns,
-        'Please delete the "Notes" column, which is the final column of the dataframe.',
-        'You can do this by selecting the column header and pressing the Delete key.'
-    ),
-    # Turn the term column into a number with the formula =VALUE(LEFT(term, 3))
-    (
-        lambda df: df["term"].dtype != "int64",
-        'Please extract the number of months from the "term" column.',
-        'To do so, double click on a cell in the column, and write the formula `=INT(LEFT(term, 3))`.'
-    ),
-]
+import numpy as np
+import matplotlib.pyplot as plt 
+import streamlit as st 
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10  # набор 60К данных 10 классов
+from tensorflow.keras.models import Sequential  # класс для создания слоев
+from tensorflow.keras.layers import Flatten, Dense  # классы для создания полносвязных слоёв
+from tensorflow.keras.utils import to_categorical  # преобразует вектор в матрицу двоичных классов
 
 
-def run_data_checks_and_display_prompts(df):
-    """
-    Runs the data checks and displays prompts for the user to fix the data.
-    """
-    for check, error_message, help in CHECKS_AND_ERRORS:
-        if check(df):
-            st.error(error_message + " " + help)
-            return False
-    return True
+def create_model():
+    (x_train, y_train), (x_val, y_val) = cifar10.load_data()
+    x_train = x_train / 255 
+    x_val = x_val / 255
+    # теперь всё от 0 до 1
+
+    y_train = to_categorical(y_train, 10)
+    y_val = to_categorical(y_val, 10)
+
+    model = Sequential([
+            Flatten(input_shape=(32, 32, 3)),
+            Dense(1000, activation='relu'), 
+            Dense(10, activation='softmax')
+        ])
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam', 
+                metrics=['accuracy'])
+
+    model.fit(x_train, y_train, batch_size=64, epochs=10, validation_data=(x_val, y_val))
+    model.save('cifar_model.h5')
 
 
+def main():
+    st.title('Классификатор на основе Cifar10 ')
+    st.write('Загрузи картинку')
 
-@st.cache_data
-def convert_df(df):
-    return df.to_csv(index=False).encode('utf-8')
+    file = st.file_uploader('Загрузи .jpg или .png', type=['jpg', 'png'])
+    if not file:
+        st.text('Это не .jpg или .png')
+    else:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        resized = image.resize((32, 32))
+        img_array = np.array(resized) / 255  # нормализуем значение пикселей
+        img_array = img_array.reshape((1, 32, 32, 3))  # изображений, размеры, каналов
+
+        model = tf.keras.models.load_model('cifar_model.h5')
+        predictions = model.predict(img_array)
+
+        classes = ('plane', 'car', 'bird', 'cat',
+                   'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+        fig, ax = plt.subplots()
+        y_pos = np.arange(len(classes))
+        ax.barh(y_pos, predictions[0], align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(classes)
+        ax.invert_yaxis()
+        ax.set_xlabel('Вероятность')
+        ax.set_title('Что это')
+
+        st.pyplot(fig)  # встраивание диаграммы в веб-приложение
 
 
-# Display the data inside of the spreadsheet so the user can easily fix data quality issues.
-dfs, _ = spreadsheet(import_folder='./data')
-
-# If the user has not yet imported data, prompt them to do so.
-if len(dfs) == 0:
-    st.info(
-        "Please import a file to begin. Click **Import** > **Import Files** and select a file from the `data` folder.")
-
-    # Don't run the rest of the app if the user hasn't imported data.
-    st.stop()
-
-# Run the checks on the data and display prompts
-df = list(dfs.values())[0]
-checks_passed = run_data_checks_and_display_prompts(df)
-
-# If the data passes all checks, allow the user to download the data
-if checks_passed:
-    st.success("All checks passed! This data is clean, and ready to be downloaded.")
-
-    csv = convert_df(df)
-
-    st.download_button(
-        "Press to Download",
-        csv,
-        "mito_verified_data.csv",
-        "text/csv",
-        key='download-csv'
-    )
+if __name__ == '__main__':
+    #create_model()
+    main()   
